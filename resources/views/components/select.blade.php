fd29131b8ba2647cd62fc2d8a58c435cd807fbf3
Luca Schäfer <71211303+TheGodlyLuzer@users.noreply.github.com>
2024-08-30 11:19:50 +0200

    fix(ui): fix select disabled state

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -4,7 +4,7 @@
         {{ $label }} @if($requiredAsterisk) <span class="text-red-600">*</span> @endif
     </label>
     <select
-        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-theme-600 sm:text-sm sm:leading-6 @if($errors->has($name)) ring-red-500 @else ring-gray-300 @endif"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-theme-600 sm:text-sm sm:leading-6 opacity-100 @if($errors->has($name)) ring-red-500 @else ring-gray-300 @endif"
         name="{{$name}}"
         id="{{$name}}"
         @if($disabled) disabled @endif
