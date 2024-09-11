95a5693d5ae8f213d8bac3903a0bc14696e904c1
Luca Schäfer <lschaefer@schaefersoft.ch>
2024-09-11 13:30:53 +0200

    fix(ui): fix menu.link padding

--- resources/views/components/menu/link.blade.php
+++ resources/views/components/menu/link.blade.php
@@ -1,10 +1,11 @@
-<div class="relative flex items-center flex-row hover:cursor-pointer hover:bg-gray-200 rounded-md ps-3 pe-1 py-1 @if($highlighted) bg-gray-100 after:absolute after:h-full after:w-1.5 after:bg-theme-500 after:-left-3 after:rounded-md @endif">
-    <div class="group flex items-center justify-between flex-row w-full h-6 relative">
-        <a href="{{$href}}" class="flex flex-row items-center w-full">
+<div class="relative flex items-center flex-row hover:cursor-pointer hover:bg-gray-200 rounded-md @if($highlighted) bg-gray-100 after:absolute after:h-full after:w-1.5 after:bg-theme-500 after:-left-3 after:rounded-md @endif">
+    <div class="group flex items-center justify-between flex-row w-full h-8 relative">
+        <a href="{{$href}}"
+           class="flex flex-row items-center h-full w-full ps-3 pe-1 py-3">
             {{$slot}}
         </a>
 
-        <div class="absolute right-0 top-50% max-w-6 max-h-6">
+        <div class="absolute right-1 top-50% max-w-6 max-h-6">
             {{ $extra ?? '' }}
         </div>
     </div>
